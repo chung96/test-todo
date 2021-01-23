1640d5d350fbc8052e45f7a9bd9f4bda7635c47a
chungpd <nh0crio96@gmail.com>
2021-01-23 10:34:07 +0700

    add

---
+++ src/components/pages/todo/TodoItem.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="todo-item">
+    <div class="d-flex">
+      <div class="d-flex align-items-center todo-item__left">
+        <app-checkbox
+          class="mr-3"
+          @change="toggleSelected"
+        />
+        <p class="todo-item__title" :title="detail.title">{{ detail.title }}</p>
+      </div>
+      <div
+        class="ml-auto nowrap"
+      >
+        <app-button
+          color="info"
+          rounded
+          size="sm"
+          @click.prevent="show()"
+        >
+          Detail
+        </app-button>
+        <app-button
+          class="ml-5"
+          color="error"
+          size="sm"
+          rounded
+          @click.prevent="remove()"
+        >
+          Remove
+        </app-button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    detail: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  data() {
+    return {
+      checked: false
+    }
+  },
+  methods: {
+    show() {
+      this.$emit('show', this.detail)
+    },
+    remove() {
+      this.$emit('remove', this.detail)
+    },
+    toggleSelected(val) {
+      this.$emit('toggleSelect', val, this.detail)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.todo-item {
+  border: 1px solid $color-dark;
+  border-radius: $border-radius-common;
+  padding: 1.5rem;
+}
+.todo-item__left {
+  overflow: hidden;
+}
+.todo-item__title {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  -ms-text-overflow: ellipsis;
+  -o-text-overflow: ellipsis;
+}
+</style>
---
+++ src/views/todo-list/add.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div class="container-fluid">
+      <div class="box" style="padding:4rem">
+        <form-add
+          @cancel="cancelForm"
+          @done="doneAdd"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import FormAdd from '@/components/pages/todo/FormAdd'
+
+export default {
+  name: 'TodoList',
+  components: {
+    FormAdd
+  },
+  data() {
+    return {
+    }
+  },
+  watch: {
+  },
+  methods: {
+    cancelForm() {
+      this.goList()
+    },
+    doneAdd() {
+      this.goList()
+    },
+    goList() {
+      this.$router.push({ name: 'TodoList' })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
